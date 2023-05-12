aa0a1ab5397342e8f07b6ed0b076f020d2de89b0
Catherine Meade <cat@heysparkbox.com>
2023-05-12 09:59:31 -0400

    feat: magic up a website

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -27,6 +28,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/html/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/public', to: '' },
+      ],
     }),
   ],
   resolve: {
